f04b9369a93494d0a0a576b306b4ecb21d44e353
Shivam <shivam1425.becse25@chitkara.edu.in>
2026-09-07 09:51:22 +0530

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch feature/data-utils
    Your branch is up to date with 'origin/feature/data-utils'.
    
    Changes to be committed:
    	modified:   src/components/ProtectedRoute.jsx
    	modified:   src/data/fallbackMovies.js
    	modified:   src/styles/forms.css
    	modified:   src/styles/responsive.css
    	modified:   src/styles/variables.css
    	modified:   src/utils/filters.js
    	modified:   src/utils/formatters.js
    	modified:   src/utils/moodMap.js
    	modified:   src/utils/storage.js

---
+++ src/utils/moodMap.js
@@ -0,0 +1,15 @@
+// Mood to movie genre mapping
+
+export const moodMap = {
+    Happy: ["Comedy", "Animation", "Family"],
+
+    Sad: ["Drama", "Romance"],
+
+    Romantic: ["Romance", "Drama"],
+
+    Excited: ["Action", "Adventure"],
+
+    Angry: ["Action", "Thriller"],
+
+    Relaxed: ["Comedy", "Animation", "Family"]
+};
